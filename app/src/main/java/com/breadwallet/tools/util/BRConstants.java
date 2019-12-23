8a2fd346eac96eefe4543ef458807243e4521c76
Zachary Price <zach.price105@gmail.com>
2019-12-22 22:04:49 -0700

    change unicode symbol to p/P

--- app/src/main/java/com/breadwallet/tools/util/BRConstants.java
+++ app/src/main/java/com/breadwallet/tools/util/BRConstants.java
@@ -110,8 +110,8 @@ public class BRConstants {
     public static final int CURRENT_UNIT_LITES = 1; // formerly CURRENT_UNIT_MBITS
     public static final int CURRENT_UNIT_LITECOINS = 2;
 
-    public static final String bitcoinLowercase = "\u0142";
-    public static final String bitcoinUppercase = "\u0141";
+    public static final String bitcoinLowercase = "p";
+    public static final String bitcoinUppercase = "P";
 
     public static final long PASS_CODE_TIME_LIMIT = TimeUnit.MILLISECONDS.convert(6, TimeUnit.DAYS);
 
